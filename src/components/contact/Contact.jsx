6683106ae8c9b199ab01f39030e9b295dc81ed9c
Mayur-nimkande-20 <mayurnimkande1920@gmail.com>
2024-01-29 18:09:35 +0530

    Contact responsiveness

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,50 +1,37 @@
 import React from "react";
-import {
-  FaEnvelope,
-  FaHandsHelping,
-  FaUserFriends,
-  FaMicrophone,
-} from "react-icons/fa";
+import { FaEnvelope, FaHandsHelping, FaUserFriends, FaMicrophone } from "react-icons/fa";
 
 const Contact = () => {
   return (
     <div id="Contact">
-      <div className="p-4 bg-black h-screen flex flex-col justify-center">
-        <p className="main-heading mt-12 bg-gradient-to-r from-[#b74b9b] to-white text-transparent bg-clip-text text-3xl font-bold text-center py-8">
-          Contact Us
-        </p>
-        <div className="grid gap-4 grid-cols-2 lg:grid-cols-4">
-          <Card
-            title="Speaking Opportunities"
-            subtitle="curation@vishwapreneur.in"
-            href="mailto:curation@vishwapreneur.in"
-            Icon={FaMicrophone}
-          />
-          <Card
-            title="Sponsorship Opportunities"
-            subtitle="edcflm.sponsorship@gmail.com"
-            href="mailto:edcflm.sponsorship@gmail.com"
-            Icon={FaHandsHelping}
-          />
-          <Card
-            title="Collaboration Opportunities"
-            subtitle="pr@vishwapreneur.in"
-            href="mailto:pr@vishwapreneur.in"
-            Icon={FaUserFriends}
-          />
-          <Card
-            title="Queries"
-            subtitle="contact@vishwapreneur.in "
-            href="mailto:contact@vishwapreneur.in "
-            Icon={FaEnvelope}
-          />
-        </div>
+      <div className="p-4 ">
+      {/* <div className="p-4 bg-black"> */}
+      <p className="main-heading bg-gradient-to-r from-[#b74b9b] to-white text-transparent bg-clip-text text-3xl font-bold text-center py-4">Contact Us</p>
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-10">
+        <Card
+          title="Speaking Opportunities"
+          subtitle="curation@vishwapreneur.in"
+          href="mailto:curation@vishwapreneur.in"
+          Icon={FaMicrophone}
+          phone={"+91 8668351747"}
+        />
+        <Card title="Sponsorship Opportunities" subtitle="edcflm.sponsorship@gmail.com " href="mailto:edcflm.sponsorship@gmail.com" Icon={FaHandsHelping} phone={"+91 9834204591"} />
+        <Card title="Collaboration Opportunities" subtitle='pr@vishwapreneur.in' href="mailto:pr@vishwapreneur.in" Icon={FaUserFriends} phone={"+91 8799842913"} />
+        <Card
+          title="Queries"
+          subtitle="contact@vishwapreneur.in"
+          href="mailto:contact@vishwapreneur.in "
+          phone="+91 9284320165"
+          Icon={FaEnvelope}
+        />
       </div>
     </div>
+    </div>
+    
   );
 };
 
-const Card = ({ title, subtitle, Icon, href }) => {
+const Card = ({ title, subtitle, Icon, href,phone }) => {
   return (
     <a
       href={href}
@@ -59,6 +46,10 @@ const Card = ({ title, subtitle, Icon, href }) => {
       </h3>
       <p className="text-slate-400 group-hover:text-violet-200 relative z-10 duration-300">
         {subtitle}
+      </p>
+      {/* <br /> */}
+      <p className="text-slate-400 group-hover:text-violet-200 relative z-10 duration-300">
+        {phone}
       </p>
     </a>
   );
--- src/components/homepage/Contact/Contact.jsx
+++ src/components/homepage/Contact/Contact.jsx
@@ -6,8 +6,8 @@ const Contact = () => {
     <div id="Contact">
       <div className="p-4 ">
       {/* <div className="p-4 bg-black"> */}
-      <p className="main-heading bg-gradient-to-r from-[#b74b9b] to-white text-transparent bg-clip-text text-3xl font-bold text-center py-8">Contact Us</p>
-      <div className="grid gap-4 grid-cols-2 lg:grid-cols-4 px-10">
+      <p className="main-heading bg-gradient-to-r from-[#b74b9b] to-white text-transparent bg-clip-text text-3xl font-bold text-center py-4">Contact Us</p>
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-10">
         <Card
           title="Speaking Opportunities"
           subtitle="curation@vishwapreneur.in"
