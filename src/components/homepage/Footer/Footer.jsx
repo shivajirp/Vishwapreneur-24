03b00ec40cf05ac8bead02ea4cd310b1eb2ab9fa
Mayur-nimkande-20 <mayurnimkande1920@gmail.com>
2024-02-01 10:00:40 +0530

    footer image change

--- src/components/homepage/Footer/Footer.jsx
+++ src/components/homepage/Footer/Footer.jsx
@@ -2,147 +2,152 @@ import React from 'react';
 import { FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa';
 import './Footer.css'
 import image from '../../../assets/Footerimg.svg'
+import image2 from '../../../assets/footer 1.png'
 
 const Footer = () => {
   return (
     <div
-      className={`footer-section   bg-black overflow-hidden`}
-    >
-      <footer className="text-white py-12 ">
-        <div
-          className={`w-full h-full  relative   scale-[100%] m-15`}
-          // style={{ backgroundImage: `url(bg-[url('https://res.cloudinary.com/di40y80dw/image/upload/v1706507461/Parallax/footer_hwcatb.webp)])` }}
+      className={` relative footer-section   `} 
+      // className={` relative footer-section border-[1px] border-slate-300   `} 
         >
-          <div className="container mx-auto flex flex-col items-center">
-            {/* Grid layout and content */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 mb-8">
-              {/* Quick Links Section */}
-              <div className="text-center ml-2 md:text-left lg:text-left xl:text-left">
-                <p
-                  className="font-bold mb-2 text-lg hover:underline"
-                  style={{
-                    backgroundImage:
-                      "linear-gradient(to right, #bb57a1, #6766b4)",
-                    WebkitBackgroundClip: "text",
-                    color: "transparent",
-                  }}
-                >
-                  Quick Links
-                </p>
-                {/* ... (rest of the Quick Links section) */}
-                <ul className="list-none">
-                  <li className="mb-2 hover:scale-105">
-                    <a href="#">Register</a>
-                  </li>
-                  <li className="mb-2 hover:scale-105">
-                    <a href="#">Our Team</a>
-                  </li>
-                  <li className="mb-2 hover:scale-105">
-                    <a href="#">Privacy Policy</a>
-                  </li>
-                  <li className="mb-2 hover:scale-105">
-                    <a href="#">Terms and Conditions</a>
-                  </li>
-                </ul>
-              </div>
+          {/* <div className='bg-gradient-to-b from-black via-transperent to-white w-fit h-fit w-'> */}
+            <img src={image2} className=' absolute  bottom-0 h-[39rem] md:h-[50rem] w-[100vw] ' style={{zIndex:"-1"}} alt="" />
+            {/* <img src={image2} className=' absolute -z-10 bottom-0 h-[39rem] md:h-[50rem] w-[100vw] ' style={{ webkitMaskImage: "-webkit-gradient(linear, top,  bottom,from(rgba(0,0,0,0)),via(rgba(0,0,0,0.5)), to(rgba(0,0,0,1)))"}} alt="" /> */}
+          {/* </div> */}
+      <footer className=" text-white py-12">
+        {/* <div className={`z-50    `} style={{ backgroundImage: `url(${image})`, width:screen, height:"20rem"}}></div> */}
+        <div className={`w-full h-full  relative m-15 `} >
 
-              {/* Follow Us Section */}
-              <div className="col-span-2 sm:col-span-1 transition ml-16 md:ml-1 lg:md-2 duration-300">
-                <p
-                  className="font-bold mb-2 text-lg transform hover:underline"
-                  style={{
-                    backgroundImage:
-                      "linear-gradient(to right, #bb57a1, #6766b4)",
-                    WebkitBackgroundClip: "text",
-                    color: "transparent",
-                  }}
-                >
-                  Follow us on
-                </p>
-                {/* ... (rest of the Follow Us section) */}
-                <div className="flex space-x-4 -ml-4">
-                  <a
-                    href="#"
-                    className="text-2xl transition duration-300 transform hover:scale-125"
-                  >
-                    <FaInstagram />
-                  </a>
-                  <a
-                    href="#"
-                    className="text-2xl transition duration-300 transform hover:scale-125"
-                  >
-                    <FaLinkedin />
-                  </a>
-                  <a
-                    href="#"
-                    className="text-2xl transition duration-300 transform hover:scale-125"
-                  >
-                    <FaTwitter />
-                  </a>
-                  <a
-                    href="#"
-                    className="text-2xl transition duration-300 transform hover:scale-125"
-                  >
-                    <FaYoutube />
-                  </a>
-                </div>
-              </div>
+        <div className="container mx-auto flex flex-col items-center">
+          {/* Grid layout and content */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 mb-8">
+            {/* Quick Links Section */}
+            <div className="text-center ml-2 md:text-left lg:text-left xl:text-left">
+              <p
+                className="font-bold mb-2 text-lg hover:underline"
+                style={{
+                  backgroundImage:
+                    "linear-gradient(to right, #bb57a1, #6766b4)",
+                  WebkitBackgroundClip: "text",
+                  color: "transparent",
+                }}
+              >
+                Quick Links
+              </p>
+              {/* ... (rest of the Quick Links section) */}
+              <ul className="list-none">
+                <li className="mb-2 hover:scale-105">
+                  <a href="#">Register</a>
+                </li>
+                <li className="mb-2 hover:scale-105">
+                  <a href="#">Our Team</a>
+                </li>
+                <li className="mb-2 hover:scale-105">
+                  <a href="#">Privacy Policy</a>
+                </li>
+                <li className="mb-2 hover:scale-105">
+                  <a href="#">Terms and Conditions</a>
+                </li>
+              </ul>
+            </div>
 
-              {/* Contact Us Section */}
-              <div className="col-span-2 sm:col-span-1 md:-ml-4 lg:-ml-4 -ml-8 md:text-left text-center">
-                <p
-                  className="font-bold mb-2 text-lg hover:underline"
-                  style={{
-                    backgroundImage:
-                      "linear-gradient(to right, #bb57a1, #6766b4)",
-                    WebkitBackgroundClip: "text",
-                    color: "transparent",
-                  }}
-                >
-                  Contact us
-                </p>
-                {/* ... (rest of the Contact Us section) */}
-                <p className="hover:scale-105">
-                  Email:<a href="mailto:edc@viit.ac.in"> edc@viit.ac.in</a>
-                </p>
-                <p className="hover:scale-105">
-                  Phone:<a href="tel:+918668351747"> +91 8668351747</a>
-                </p>
-                <p className="hover:scale-105">
-                  Address:
-                  <a href="https://maps.app.goo.gl/XJaQ351o7yRLJFrHA">
-                    {" "}
-                    EDC HQ, VIIT, Pune
-                  </a>
-                </p>
-              </div>
-
-              {/* Download App Section */}
-              <div className="text-center transition duration-300 transform">
-                <p
-                  className="font-bold mb-2 text-lg"
-                  style={{
-                    backgroundImage:
-                      "linear-gradient(to right, #bb57a1, #6766b4)",
-                    WebkitBackgroundClip: "text",
-                    color: "transparent",
-                  }}
-                >
-                  Download Our App
-                </p>
-                {/* ... (rest of the Download App section) */}
+            {/* Follow Us Section */}
+            <div className="col-span-2 sm:col-span-1 transition ml-16 md:ml-1 lg:md-2 duration-300">
+              <p
+                className="font-bold mb-2 text-lg transform hover:underline"
+                style={{
+                  backgroundImage:
+                    "linear-gradient(to right, #bb57a1, #6766b4)",
+                  WebkitBackgroundClip: "text",
+                  color: "transparent",
+                }}
+              >
+                Follow us on
+              </p>
+              {/* ... (rest of the Follow Us section) */}
+              <div className="flex space-x-4 -ml-4">
                 <a
                   href="#"
-                  className="text-lg border-b border-white hover:border-opacity-50 "
+                  className="text-2xl transition duration-300 transform hover:scale-125"
                 >
-                  <span className="flex items-center hover:scale-105">
-                    <i className="fab fa-google-play pr-2"></i>
-                    <h6>Download from Playstore</h6>
-                  </span>
+                  <FaInstagram />
+                </a>
+                <a
+                  href="#"
+                  className="text-2xl transition duration-300 transform hover:scale-125"
+                >
+                  <FaLinkedin />
+                </a>
+                <a
+                  href="#"
+                  className="text-2xl transition duration-300 transform hover:scale-125"
+                >
+                  <FaTwitter />
+                </a>
+                <a
+                  href="#"
+                  className="text-2xl transition duration-300 transform hover:scale-125"
+                >
+                  <FaYoutube />
                 </a>
               </div>
             </div>
+
+            {/* Contact Us Section */}
+            <div className="col-span-2 sm:col-span-1 md:-ml-4 lg:-ml-4 -ml-8 md:text-left text-center">
+              <p
+                className="font-bold mb-2 text-lg hover:underline"
+                style={{
+                  backgroundImage:
+                    "linear-gradient(to right, #bb57a1, #6766b4)",
+                  WebkitBackgroundClip: "text",
+                  color: "transparent",
+                }}
+              >
+                Contact us
+              </p>
+              {/* ... (rest of the Contact Us section) */}
+              <p className="hover:scale-105">
+                Email:<a href="mailto:edc@viit.ac.in"> edc@viit.ac.in</a>
+              </p>
+              <p className="hover:scale-105">
+                Phone:<a href="tel:+918668351747"> +91 8668351747</a>
+              </p>
+              <p className="hover:scale-105">
+                Address:
+                <a href="https://maps.app.goo.gl/XJaQ351o7yRLJFrHA">
+                  {" "}
+                  EDC HQ, VIIT, Pune
+                </a>
+              </p>
+            </div>
+
+            {/* Download App Section */}
+            <div className="text-center transition duration-300 transform">
+              <p
+                className="font-bold mb-2 text-lg"
+                style={{
+                  backgroundImage:
+                    "linear-gradient(to right, #bb57a1, #6766b4)",
+                  WebkitBackgroundClip: "text",
+                  color: "transparent",
+                }}
+              >
+                Download Our App
+              </p>
+              {/* ... (rest of the Download App section) */}
+              <a
+                href="#"
+                className="text-lg border-b border-white hover:border-opacity-50 "
+              >
+                <span className="flex items-center hover:scale-105">
+                  <i className="fab fa-google-play pr-2"></i>
+                  <h6>Download from Playstore</h6>
+                </span>
+              </a>
+            </div>
           </div>
+        </div>
         </div>
       </footer>
     </div>
